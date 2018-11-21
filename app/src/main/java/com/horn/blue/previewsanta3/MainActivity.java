bff38db21ff0ffee5f875c46fdd3777a4b262219
alpi2111 <carlos.alpi21@gmail.com>
2018-11-21 03:22:36 -0600

    Splash Screen F

--- app/src/main/java/com/horn/blue/previewsanta3/MainActivity.java
+++ app/src/main/java/com/horn/blue/previewsanta3/MainActivity.java
@@ -1,15 +1,51 @@
 package com.horn.blue.previewsanta3;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME = 5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!cargarPref()) {
+                    Intent intentHome = new Intent(getApplicationContext(), HomeLoginActivity.class);
+                    startActivity(intentHome);
+                    finish();
+                } else {
+                   /* Intent intent = new Intent(getApplicationContext(), HomeFinal.class);
+                    startActivity(intent);
+                    finish();*/
+                }
+            }
+        }, SPLASH_TIME);
+    }
+
+    private void guardarIsLogin(String x) {
+        SharedPreferences sharedPreferences = getSharedPreferences("Credenciales", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("isLogin", x);
+        editor.commit();
+    }
+
+    private boolean cargarPref() {
+        SharedPreferences sharedPreferences = getSharedPreferences("Credenciales", Context.MODE_PRIVATE);
+        if (sharedPreferences.getString("isLogin", "No").equals("Yes")) {
+            return true;
+        }
+        return false;
     }
 }
