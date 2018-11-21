81c63493bba4fd23c147367fc00c06a09d78a13b
alpi2111 <carlos.alpi21@gmail.com>
2018-11-21 04:09:15 -0600

    Home Login to Login-Register. Fun and Arrob back

--- app/src/main/java/com/horn/blue/previewsanta3/HomeLoginActivity.java
+++ app/src/main/java/com/horn/blue/previewsanta3/HomeLoginActivity.java
@@ -1,13 +1,37 @@
 package com.horn.blue.previewsanta3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeLoginActivity extends AppCompatActivity {
+
+    private Button btnLogin, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeLoginActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeLoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
