a6f60d1236ab91a5f7c5a1b5df3d2b4d5654b88c
alpi2111 <carlos.alpi21@gmail.com>
2018-11-21 03:57:52 -0600

    Other NOOO

--- app/src/main/java/com/horn/blue/previewsanta3/MainActivity.java
+++ app/src/main/java/com/horn/blue/previewsanta3/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         //guardarIsLogin("No");
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
